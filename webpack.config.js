d3a0edd8212176d69fbe5e303fac8cb6c40816e5
GenWatt <adrian.nowy@op.pl>
2020-04-09 18:37:45 +0200

    Add

--- webpack.config.js
+++ webpack.config.js
@@ -6,29 +6,34 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "https://GenWatt.github.io/Wyszukiwarka",
+    publicPath: "https://GenWatt.github.io/Wyszukiwarka/dist",
   },
   module: {
-    rules: [{
-      test: /\.s[ac]ss$/i,
-      use: [
-        // Creates `style` nodes from JS strings
-        'style-loader',
-        // Translates CSS into CommonJS
-        'css-loader',
-        // Compiles Sass to CSS
-        'sass-loader',
-      ],
-    }, {
-      test: /\.js$/,
-      loader: 'buble-loader',
-      include: path.join(__dirname, 'src'),
-      options: {
-        objectAssign: 'Object.assign'
-      }
-    }],
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: "buble-loader",
+        include: path.join(__dirname, "src"),
+        options: {
+          objectAssign: "Object.assign",
+        },
+      },
+    ],
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: "./index.html"
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./se.html",
+    }),
+  ],
 };
